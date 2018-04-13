5444382f2d80e57c917a7bc1eeb957d65211293a
G <gigiaboone@yahoo.com>
2018-04-13 02:28:33 -0400

    Wrote a little more on the auto-dodge

--- Renny/modules/autododge.cpp
+++ Renny/modules/autododge.cpp
@@ -15,6 +15,14 @@ float signOfXDifference(Vector firstVector, Vector secondVector) {
 	return NULL;
 }
 
+float findClosestPoint(float slope, float xPos, float targetXPos, Vector targetObjPos) {
+	float closestPoint = 1500000.0f;
+	while (xPos < targetXPos) {
+		
+
+	}
+}
+
 void autododge(object* myPlayer) {
 	if (spellCastData != NULL) {
 		float distanceApart = absVectorDistance(myPlayer->mUnitPos, objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos);
@@ -22,10 +30,11 @@ void autododge(object* myPlayer) {
 			float slope = (spellCastData->mTargetPosition.z - objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos.z) / (spellCastData->mTargetPosition.x - objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos.x);
 			float distance = signOfXDifference(myPlayer->mUnitPos, objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos);
 			float xPos = objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos.x + distance;
-			float zPos = (objMgr->mObjectManagerArray[spellCastData->casterIndex]->mUnitPos.x + distance) * slope;
+			float zPos = (xPos) * slope;
+			
 			
 			//Check the points on the slope minus the missile width to see what points are closest to the center of the object being aimed at
-
+			//Run through the slope 
 				//spellCastData->mSpellData->mSpellInfo->mMissileWidth
 		}
 	}
